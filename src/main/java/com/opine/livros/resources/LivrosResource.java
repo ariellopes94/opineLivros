b1863eb792d37ed8cd19a3209d47302a6e4f4d9a
Ariel Lopes <ariel.edit@gmail.com>
2020-05-26 09:38:50 -0400

    Criando uma representação para o recurso Livro

--- src/main/java/com/opine/livros/resources/LivrosResource.java
+++ src/main/java/com/opine/livros/resources/LivrosResource.java
@@ -1,16 +1,30 @@
 package com.opine.livros.resources;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.opine.livros.domain.Livros;
 
 @RestController
 @RequestMapping("/livros")
 public class LivrosResource {
 
 	@GetMapping
-	public String listar() {
-		return "Hello word";
+	public List<Livros> listar() {
+		
+		Livros l1 = new Livros("Aprenda Java");
+		Livros l2 = new Livros("Rest aplicado");
+		Livros l3 = new Livros("Git passo-a-passo");
+
+		List<Livros> livros = new  ArrayList();
+		
+		livros.addAll(Arrays.asList(l1,l2,l3));
+		return livros;
 	}
 
 }
