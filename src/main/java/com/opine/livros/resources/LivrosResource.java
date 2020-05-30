e775fdb4710e87f7cf1782d58e622373bae26855
Ariel Lopes <ariel.edit@gmail.com>
2020-05-29 20:27:55 -0400

    Metodo Put , atualizando

--- src/main/java/com/opine/livros/resources/LivrosResource.java
+++ src/main/java/com/opine/livros/resources/LivrosResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +46,12 @@ public class LivrosResource {
 	public void deletarLivro(@PathVariable Long id) {
 		
 		livroRepository.deleteById(id);
+	}
+	
+	@PutMapping(value = "/{id}")
+	public Livros atualizarLivro(@RequestBody Livros livros, @PathVariable Long id) {
+		livros.setId(id);
+		return livroRepository.save(livros);
 	}
 	
 	
