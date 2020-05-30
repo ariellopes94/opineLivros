11595877c8972623777e6ed7ea6c9a34c26454e9
Ariel Lopes <ariel.edit@gmail.com>
2020-05-30 11:56:11 -0400

    Tratamento de retorno do Status, Salvar create 201, buscar id que nao existe 204 object not found

--- src/main/java/com/opine/livros/resources/LivrosResource.java
+++ src/main/java/com/opine/livros/resources/LivrosResource.java
@@ -1,13 +1,17 @@
 package com.opine.livros.resources;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import javax.security.auth.message.callback.PrivateKeyCallback.Request;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ServerProperties.Servlet;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +19,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.opine.livros.domain.Livros;
 import com.opine.livros.repositories.LivroRepository;
@@ -27,28 +33,39 @@ public class LivrosResource {
 	@Autowired
 	LivroRepository livroRepository;
 	
-	@GetMapping
+	@GetMapping                  //Buscar Todos
 	public List<Livros> listar() {
 		return livroRepository.findAll();
 	}
 	
-	@PostMapping
-	public Livros salvar(@RequestBody Livros livros) {
-		return livroRepository.save(livros);
+	@PostMapping                   //Salvar
+	public ResponseEntity<Livros> salvar(@RequestBody Livros livros) {
+		 Livros obj = livroRepository.save(livros);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").
+				buildAndExpand(obj.getId()).toUri();
+
+		return ResponseEntity.created(uri).build();
+		
+		 
 	}
 
-	@GetMapping(value= "/{id}")
-	public Optional<Livros> buscarPorId(@PathVariable Long id ) {
-		return livroRepository.findById(id);
+	@GetMapping(value= "/{id}")    //Buscar por ID
+	public ResponseEntity<Livros> buscarPorId(@PathVariable Long id ) {
+		Optional<Livros> livros = livroRepository.findById(id);
+		     Livros obj=  livros.orElse(null);
+		     if(obj == null) {
+		     return ResponseEntity.notFound().build();
+			}
+		   return  ResponseEntity.ok().body(obj);
 	}
 	
-	@DeleteMapping(value = "{id}")
+	@DeleteMapping(value = "{id}")  //Deletar por ID
 	public void deletarLivro(@PathVariable Long id) {
 		
 		livroRepository.deleteById(id);
 	}
 	
-	@PutMapping(value = "/{id}")
+	@PutMapping(value = "/{id}")       //Atualizar
 	public Livros atualizarLivro(@RequestBody Livros livros, @PathVariable Long id) {
 		livros.setId(id);
 		return livroRepository.save(livros);
