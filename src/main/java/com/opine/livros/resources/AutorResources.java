91d61c2e712197a35b230207f5196f4d945365de
Ariel Lopes <ariel.edit@gmail.com>
2020-06-07 16:48:51 -0400

    Negociação de conteúdos com Media Type; para aceitar JSON e XML

--- src/main/java/com/opine/livros/resources/AutorResources.java
+++ src/main/java/com/opine/livros/resources/AutorResources.java
@@ -1,10 +1,12 @@
 package com.opine.livros.resources;
 
+
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -25,7 +28,33 @@ public class AutorResources {
 	@Autowired
 	private AutorService autorService;
 	
-	@GetMapping
+	 
+	   /*
+	    * Negociação de conteúdos com Media Type.
+	    * 
+	    * 
+	    * @RequestMapping(method = RequestMethod.GET , produces = {             
+	    * MediaType.APPLICATION_JSON_VALUE , MediaType.APPLICATION_XML_VALUE 
+	    *
+	    *ADICIONADR DEPEDENCIA NO POM
+	    *POM
+	    *
+	    *<dependency>
+	    *    <groupId> com.fasterxml.jackson.dataformat</groupId>
+	    *    <artifactId>jackson-dataformat-xml</artifactId>
+	    *</dependency>
+	    *  </dependencies>
+	    *
+	    *
+	    * Postman -> Headers -> 
+        *  content-Type               application/json   | ACEITA JSON
+        * Accept                     application/xml   | ACEITA XML
+	    */
+	
+	@RequestMapping(method = RequestMethod.GET , produces = {              //ACEITAR RETORNO PARA JSON E XML 
+	   MediaType.APPLICATION_JSON_VALUE , MediaType.APPLICATION_XML_VALUE  //Tem que adicionar a Depecencia do XML no POM
+	  
+	})
 	public ResponseEntity<List<Autor>> listar(){
 		List<Autor> autores = autorService.buscarTodosAutores();
 		
